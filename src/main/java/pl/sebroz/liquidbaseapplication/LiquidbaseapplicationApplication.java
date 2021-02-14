8e307d1363a8a54c25f4c67b6bc03e8453b7c890
Sebastian Różyczka <rozyczka.sebastian@gmail.com>
2021-02-14 14:34:52 +0100

    project base

---
+++ src/main/java/pl/sebroz/liquidbaseapplication/LiquidbaseapplicationApplication.java
@@ -0,0 +1,13 @@
+package pl.sebroz.liquidbaseapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiquidbaseapplicationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiquidbaseapplicationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/sebroz/liquidbaseapplication/LiquidbaseapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sebroz.liquidbaseapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiquidbaseapplicationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
